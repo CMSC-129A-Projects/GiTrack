f7734275ee7761a5798e86ef518ef2f40646b6ec
Yul Vincent Sullano <yesullano@gmail.com>
2021-04-27 13:19:16 +0800

    docs: backend routes
    * feature(task) : add task endpoint, add task to database
    
    * fix: remove columns to remove bug
    
    * docs(insomnia): add insomnia data
    
    * docs(insomnia): add insomnia data
    
    * package-lock.json
    
    * .env
    
    * .env
    
    * package.json
    
    * feature(models): removed unused var
    
    * features(tasks-route): fixed prettier errors
    
    * feature(task): remove task endpoint, rework SQL queries
    
    * fix: sql queries to prevent sqli
    
    * fix: remove node.js
    
    * fis: re-enable prettier, remove quoation marks from sql queries
    
    * test/board-endpoints : add board endpoint testing, rework getPermissions for route interaction
    
    * test/board-endpoints: fix no-unused-vars
    
    * test/board-endpoint: fix no-unused-var
    
    * test/board-endpoints: fix no-unused-var
    
    * test/board-endpoints: add test for /delete-board route, rework getBoardById to return board instead of title
    
    * test/board-endpoints: added test for /delete-board route
    
    * docs(board): add booard routes documentation
    
    Co-authored-by: Oscar Vian Valles <oscarvianvalles@gmail.com>
    Co-authored-by: Yul Sullano <yesullano@up.edu.ph>

--- backend/routes/boards.js
+++ backend/routes/boards.js
@@ -23,6 +23,64 @@ router.get('/', authJWT, (req, res) => {
   res.json({ error_message: req.user });
 });
 
+/**
+ * @swagger
+ * tags:
+ *  name: Boards
+ *  description: Perform operations on boards
+ */
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    CreateBoard:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *          nullable: true
+ *          description: Board ID
+ *          example: 1
+ *        error_message:
+ *          type: string
+ *          nullable: true
+ *          description: Specific error message causing the error
+ *          example: MISSING_TITLE
+ *
+ * /boards/create-board:
+ *   post:
+ *     summary: Create a board.
+ *     tags: [Boards]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: Board title
+ *                 example: GiTrack
+ *               id:
+ *                 type: integer
+ *                 description: User ID
+ *                 example: 1
+ *     responses:
+ *       201:
+ *         description: Board created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ *       400:
+ *         description: Error parsing request body
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ */
 router.post('/create-board', authJWT, async (req, res) => {
   const { title } = req.body;
   const { id: userId } = req.user;
@@ -42,6 +100,54 @@ router.post('/create-board', authJWT, async (req, res) => {
     return res.status(500).json({ id: null, error_message: err });
   }
 });
+
+/**
+ * @swagger
+ *  /boards/edit-board:
+ *   patch:
+ *     summary: Edit a board.
+ *     tags: [Boards]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: New board title
+ *                 example: GiTrack
+ *               id:
+ *                 type: integer
+ *                 description: Board ID
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Board successfully edited
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ *       400:
+ *         description: Error parsing request body
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ *       403:
+ *         description: User lacking permissions to perform current action
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ *       409:
+ *         description:  Current name and new name are the same
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ */
 
 router.patch('/edit-board', authJWT, async (req, res) => {
   const { id, name } = req.body;
@@ -82,7 +188,7 @@ router.patch('/edit-board', authJWT, async (req, res) => {
   }
 
   try {
-    const boardId = await editBoard(id, name, userId);
+    const boardId = await editBoard(id, name);
 
     return res.json({ id: boardId, title: name, error_message: null });
   } catch (err) {
@@ -90,6 +196,48 @@ router.patch('/edit-board', authJWT, async (req, res) => {
     return res.status(500).json({ id: null, error_message: err });
   }
 });
+
+/**
+ * @swagger
+ *  /boards/delete-board:
+ *   delete:
+ *     summary: Delete a board.
+ *     tags: [Boards]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 description: Board ID
+ *                 example: 1
+ *               userId:
+ *                 type: integer
+ *                 description: User ID
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Board successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ *       400:
+ *         description: Error parsing request body
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ *       403:
+ *         description: User lacking permissions to perform current action
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateBoard'
+ */
 
 router.delete('/delete-board', authJWT, async (req, res) => {
   const { id } = req.body;
@@ -136,6 +284,60 @@ router.delete('/delete-board', authJWT, async (req, res) => {
     return res.status(500).json({ id: null, error_message: err });
   }
 });
+
+/**
+ * @swagger
+ *  /boards/get-board:
+ *   get:
+ *     summary: Get boards of which user with id userId is a member.
+ *     tags: [Boards]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: integer
+ *                 description: User ID
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Board successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 boardId:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       nullable: false
+ *                       description: Board ID
+ *                       example: 1
+ *                   nullable: false
+ *                   description: Collection of Board IDs of boards where the user is a member
+ *       400:
+ *         description: Error parsing request body
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 boardId:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       nullable: false
+ *                       description: Board ID
+ *                       example: 1
+ *                   nullable: false
+ *                   description: Collection of Board IDs of boards where the user is a member
+ */
 
 router.get('/get-board', authJWT, async (req, res) => {
   const { id: userId } = req.body;
