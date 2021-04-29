c399b40f66391b2786fdc7435511ff463105891c
Justin Po <justin.andre.po@gmail.com>
2021-04-29 15:50:32 +0800

    feat: connect add board api to frontend

--- frontend/src/routes/Board/Add/board-add.jsx
+++ frontend/src/routes/Board/Add/board-add.jsx
@@ -1,21 +1,38 @@
 /** @jsxImportSource @emotion/react */
+import { useForm } from 'react-hook-form';
 
 import Button from 'components/Button';
 import buttonVariants from 'components/Button/constants';
+
+import BoardService from 'services/BoardService';
 
 // Style
 import * as style from './board-add-styles';
 
 export default function AddBoard() {
+  const { register, watch, handleSubmit } = useForm();
+  const watchTitle = watch('title', '');
+
+  const onSubmit = (formData) => {
+    BoardService.create({ body: formData });
+  };
+
   return (
-    <div css={style.addBoard}>
+    <form css={style.addBoard} onSubmit={handleSubmit(onSubmit)}>
       <input
         css={style.addBoard_input}
         type="text"
         placeholder="Type board name here..."
-        name="name"
+        {...register('title', { required: true })}
       />
-      <Button variant={buttonVariants.LARGE.PRIMARY}>Create Board</Button>
-    </div>
+      <Button
+        variant={buttonVariants.LARGE.PRIMARY}
+        type="submit"
+        onClick={() => {}}
+        disabled={watchTitle === ''}
+      >
+        Create Board
+      </Button>
+    </form>
   );
 }
