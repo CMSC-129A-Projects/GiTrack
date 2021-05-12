bc3c76acb65e271d5594749769c365f1ffe79f18
PatTumulak <tumulakpatricia@gmail.com>
2021-05-13 01:45:26 +0800

    feat(frontend): add Add Developer UI

---
+++ frontend/src/widgets/AddDeveloperModal/add-developer-modal-styles.js
@@ -0,0 +1,5 @@
+import { css } from '@emotion/react';
+
+export const addDeveloperModal_input = css`
+  margin: 0 0 16px 0;
+`;
---
+++ frontend/src/widgets/AddDeveloperModal/add-developer-modal.jsx
@@ -0,0 +1,35 @@
+/** @jsxImportSource @emotion/react */
+
+import Modal from 'components/Modal';
+import Input from 'components/Input';
+import buttonVariants from 'components/Button/constants';
+import modalSizes from 'components/Modal/constants';
+
+// Style
+import * as style from './add-developer-modal-styles';
+
+export default function AddDeveloperModal({ isOpen, handleClose }) {
+  return (
+    <Modal
+      size={modalSizes.SM}
+      title="Add Repository"
+      icon="person_add"
+      isOpen={isOpen}
+      handleClose={handleClose}
+      actions={[
+        {
+          name: 'Add',
+          onClick: () => {},
+          variant: buttonVariants.SMALL.PRIMARY,
+        },
+        {
+          name: 'Cancel',
+          onClick: handleClose,
+          variant: buttonVariants.SMALL.SECONDARY,
+        },
+      ]}
+    >
+      <Input css={style.addDeveloperModal_input} label="Email address" />
+    </Modal>
+  );
+}
