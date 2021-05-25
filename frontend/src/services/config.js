6b52d030e6f68ca685b52ff419070e811eac8972
Justin Po <justin.andre.po@gmail.com>
2021-05-25 19:38:28 +0800

    feat: update axios config

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
+
+export const isLocal = () => window.location.href.includes('localhost');
+
 const config = {
-  API_URL: 'http://localhost:3000',
-  BASE_URL: 'http://localhost:3001',
+  API_URL: isLocal() ? 'http://localhost:3000' : 'https://api.gitrack.codes',
+  BASE_URL: window.location.origin,
 };
 
 export default config;
