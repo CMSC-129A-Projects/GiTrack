e0a796f126d241af8b57002bdcd176205569a02a
Oscar Vian Valles <oscarvianvalles@gmail.com>
2021-06-19 15:55:03 +0800

    docs: add docs to models/assignees and remove unused function

--- backend/models/assignees.js
+++ backend/models/assignees.js
@@ -3,6 +3,10 @@ const dbHandler = require('../db');
 
 const { assignee: assigneeErrorMessages } = require('../constants/error-messages');
 
+/**
+ * Get the assignees of a task based on the ID of a task
+ * @param {number} taskId - The ID of the task
+ */
 async function getAssignees(taskId) {
   const db = await dbHandler;
 
@@ -21,6 +25,11 @@ async function getAssignees(taskId) {
   }
 }
 
+/**
+ * Set the assignees of a task based on the ID of the task and the IDs of the assignees
+ * @param {number} taskId - The ID of the task
+ * @param {Array.<number>} assigneeIds - The IDs of the assignees
+ */
 async function setAssignees(taskId, assigneeIds) {
   const db = await dbHandler;
 
@@ -48,20 +57,29 @@ async function setAssignees(taskId, assigneeIds) {
   }
 }
 
+/**
+ * Replace all assignees of a task with the new assignees
+ * @param {number} taskId - The ID of the task
+ * @param {Array.<number>} assigneeIds - The IDs of the assignees
+ */
 async function replaceAssignees(taskId, assigneeIds) {
   const db = await dbHandler;
 
   try {
+    // Get the current users
     const currentUsers = (
       await db.all('SELECT user_id FROM Assignees WHERE task_id = ?', taskId)
     ).map((user) => user.user_id);
 
+    // Get the additional users assigned to the task
     const newUsers = assigneeIds.filter((assignee) => !currentUsers.includes(assignee));
 
+    // Get the users that are removed as assignatory for the task
     const removedUsers = currentUsers.filter(
       (assignee) => !assigneeIds.includes(assignee)
     );
 
+    // Add all new assignees to the db
     const assign = await db.prepare(
       'INSERT INTO Assignees (task_id, user_id) VALUES (?, ?)'
     );
@@ -77,6 +95,7 @@ async function replaceAssignees(taskId, assigneeIds) {
         await assign.finalize();
       });
 
+    // Remove all users that are removed as an assignatory for the task
     const remove = await db.prepare(
       'DELETE FROM Assignees WHERE task_id = ? AND user_id = ?'
     );
@@ -97,20 +116,8 @@ async function replaceAssignees(taskId, assigneeIds) {
   }
 }
 
-async function isUserAssignedInTask(taskId, userId) {
-  const db = await dbHandler;
-
-  const user = await db.get(
-    'SELECT user_id FROM Assignees WHERE task_id = ? AND user_id = ?',
-    taskId,
-    userId
-  );
-  return user;
-}
-
 module.exports = {
   getAssignees,
   setAssignees,
   replaceAssignees,
-  isUserAssignedInTask,
 };
